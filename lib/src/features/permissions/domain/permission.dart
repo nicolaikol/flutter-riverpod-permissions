961dcd073bb3b590329df7dbbdbfafcae558e49a
nicolaikol <nicolai.kolandjian@gmail.com>
2026-03-24 21:51:41 +0100

    Removed Freezed dependencies

--- lib/src/features/permissions/domain/permission.dart
+++ lib/src/features/permissions/domain/permission.dart
@@ -1,16 +1,30 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-part 'permission.freezed.dart';
+class Perm {
+  const Perm({required this.status});
 
-@freezed
-abstract class Perm with _$Perm {
-  const Perm._();
-  const factory Perm({required PermissionStatus status}) = _Perm;
+  final PermissionStatus status;
 
   // A word on: status.isDenied
   //  - It's possible that we status.isPermanentlyDenied, but we don't know for sure until we do a request();
   //  - Therefore needsOpenSettings can fail to identify the truth.
   bool get needsOpenSettings => status.isPermanentlyDenied || status.isLimited;
   bool get isAvailable => status.isGranted || status.isLimited;
+
+  Perm copyWith({PermissionStatus? status}) {
+    return Perm(status: status ?? this.status);
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Perm &&
+          runtimeType == other.runtimeType &&
+          status == other.status;
+
+  @override
+  int get hashCode => status.hashCode;
+
+  @override
+  String toString() => 'Perm(status: $status)';
 }
